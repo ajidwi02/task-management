0985bf40aff76e03543eeb1f64773718d53dcabf
Aji Dwi Prakoso <ajidwi111202@gmail.com>
2025-10-17 13:44:48 +0700

    initialize cors & update .env file

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
